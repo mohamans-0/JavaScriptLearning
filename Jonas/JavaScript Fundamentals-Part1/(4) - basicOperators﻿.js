1e1c284df2fd39003492e6949fd4db60da59c8f4
mohamans-0 <153182781+mohamans-0@users.noreply.github.com>
2024-09-11 20:44:19 +0300

    Update (4) - basicOperators﻿.js

--- Jonas/JavaScript Fundamentals-Part1/(4) - basicOperators﻿.js
+++ Jonas/JavaScript Fundamentals-Part1/(4) - basicOperators﻿.js
@@ -1,1 +1,32 @@
+const country = "Jordan";
+const continent = "Asia";
+let population = 8;
 
+const isIsland = false;
+//isIsland = true; // error , variable const is immutable
+let language;
+
+language = "arabic";
+
+let populationFinland = 6;
+let populationAverage = 33;
+
+population = population / 2;
+population++;
+
+console.log(population);
+
+console.log(population > populationFinland);
+console.log(population > populationAverage);
+
+("Portugal is in Europe, and its 11 million people speak portuguese");
+
+console.log(
+  country +
+    " is in " +
+    continent +
+    " , and its " +
+    population +
+    " million people speak " +
+    language
+);
