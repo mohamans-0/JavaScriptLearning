49b0bca6f97e15a138a5b49f97a63dfb7bdf86f4
mohamans-0 <153182781+mohamans-0@users.noreply.github.com>
2024-09-11 14:58:57 +0300

    Update ass2.js

--- Jonas/JavaScript Fundamentals-Part1/ass2.js
+++ Jonas/JavaScript Fundamentals-Part1/ass2.js
@@ -1,2 +1,13 @@
+let country = "Jordan";
+let continent = "Asia";
+let population = "8 million";
+
 let isIsland = false;
 let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
