2b33ca7a06c31f1caf5bb589fc183f6159f1d947
mohamans-0 <153182781+mohamans-0@users.noreply.github.com>
2024-09-11 15:27:59 +0300

    Update ass3.js

--- Jonas/JavaScript Fundamentals-Part1/ass3.js
+++ Jonas/JavaScript Fundamentals-Part1/ass3.js
@@ -1,1 +1,10 @@
+const country = "Jordan";
+const continent = "Asia";
+let population = "8 million";
+
+const isIsland = false;
+isIsland = true ; // error , variable const is immutable  
+let language;
+
+language = "arabic";
 
