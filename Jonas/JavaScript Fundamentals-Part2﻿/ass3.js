c83d0966b827c985aa99e8c04c01d896c95d29e5
mohamans-0 <153182781+mohamans-0@users.noreply.github.com>
2024-09-29 15:28:48 +0300

    Create ass3.js

---
+++ Jonas/JavaScript Fundamentals-Part2﻿/ass3.js
@@ -0,0 +1,7 @@
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const percUSA = percentageOfWorld3(332);
+const percChina = percentageOfWorld3(1441);
+const percPortugal = percentageOfWorld3(10);
+
+console.log(percUSA, percChina, percPortugal);
