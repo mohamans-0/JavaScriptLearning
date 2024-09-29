ebe78dce636c43b6c0551af93e741dc733e98d44
mohamans-0 <153182781+mohamans-0@users.noreply.github.com>
2024-09-29 15:15:53 +0300

    Update and rename ass1 to ass1.js

---
+++ Jonas/JavaScript Fundamentals-Part2﻿/ass1.js
@@ -0,0 +1,7 @@
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has a ${population} million people and its capital city is ${capitalCity} `;
+}
+
+let l = describeCountry("Jordan", 10, "amman");
+console.log(l);
